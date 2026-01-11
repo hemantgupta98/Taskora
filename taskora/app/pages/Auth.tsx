8d6ac433497c2100e0eb16ac85e08600c3c3f43a
HEMANT GUPTA <hemantgupta8800@gmail.com>
2026-01-11 19:43:52 +0530

    working on dark mode theme

--- taskora/app/pages/Auth.tsx
+++ taskora/app/pages/Auth.tsx
@@ -2,10 +2,10 @@ import AuthForm from "../components/ui/AuthForm";
 
 export default function Auth() {
   return (
-    <div className="min-h-screen flex items-center justify-center p-6">
-      <div className="bg-white rounded-2xl shadow-2xl max-w-5xl w-full grid md:grid-cols-2 overflow-hidden">
+    <div className="min-h-screen flex items-center justify-center p-6 bg-pink-50 dark:bg-slate-900">
+      <div className="bg-white dark:bg-slate-800 rounded-2xl shadow-2xl max-w-5xl w-full grid md:grid-cols-2 overflow-hidden">
         {/* Left Illustration */}
-        <div className="hidden md:flex items-center justify-center bg-gradient-to-br from-slate-900 to-slate-800 p-10">
+        <div className="hidden md:flex items-center justify-center bg-gradient-to-br from-slate-900 to-slate-800 dark:from-slate-700 dark:to-slate-600 p-10">
           <img src="/network.png" alt="Illustration" className="max-w-sm" />
         </div>
 
