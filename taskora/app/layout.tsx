faf6f24013c7db1851fcce73855bf2861ef1f521
HEMANT GUPTA <hemantgupta8800@gmail.com>
2026-01-18 16:34:48 +0530

    minnor updates

--- taskora/app/layout.tsx
+++ taskora/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Navbar from "./components/navbar";
 import Sidebar from "./components/sidebar";
+import Auth from "./components/Auth";
 
 import "./globals.css";
 const geistSans = Geist({
@@ -26,12 +27,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="flex h-screen overflow-hidden">
-        <Sidebar />
-        <div className="flex-1 flex flex-col">
-          <Navbar />
-          <main className="flex-1 overflow-y-auto p-6">{children}</main>
-        </div>
+      <body>
+        <Auth />
+        <main>{children}</main>
       </body>
     </html>
   );
