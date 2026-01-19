3ae3a21c5854e9136052c2fbca5d152dfbb38945
HEMANT GUPTA <hemantgupta8800@gmail.com>
2026-01-19 19:21:09 +0530

    setup animation on auth page

--- taskora/app/components/Auth.tsx
+++ taskora/app/components/Auth.tsx
@@ -1,17 +1,44 @@
+"use client";
 import AuthForm from "../components/ui/AuthForm";
 import Image from "next/image";
+import { motion, stagger } from "motion/react";
 
 export default function Auth() {
   return (
     <div className="min-h-screen flex items-center justify-center p-6  dark:bg-slate-900">
       <div className="bg-white dark:bg-slate-800 rounded-2xl shadow-2xl max-w-5xl w-full grid md:grid-cols-2 overflow-hidden">
         {/* Left Illustration */}
-        <div className="hidden md:flex items-center justify-center bg-gradient-to-br from-slate-900 to-slate-800 dark:from-slate-700 dark:to-slate-600 p-10">
+        <motion.div
+          initial={{ opacity: 0, y: 0, x: -100 }}
+          whileInView={{ opacity: 1, y: 0, x: 0 }}
+          viewport={{ once: true }}
+          transition={{
+            type: "spring",
+            stiffness: 100,
+            damping: 80,
+            duration: 5,
+            delay: 2,
+          }}
+          className="hidden md:flex items-center justify-center bg-gradient-to-br from-slate-900 to-slate-800 dark:from-slate-700 dark:to-slate-600 p-10"
+        >
           <div>
             <Image src="/logo.png" alt="logo" height={50} width={50} />
           </div>
-        </div>
-        <AuthForm />
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: 0, x: 100 }}
+          whileInView={{ opacity: 1, y: 0, x: 0 }}
+          viewport={{ once: true }}
+          transition={{
+            type: "spring",
+            stiffness: 100,
+            damping: 80,
+            duration: 5,
+            delay: 2,
+          }}
+        >
+          <AuthForm />
+        </motion.div>
       </div>
     </div>
   );
