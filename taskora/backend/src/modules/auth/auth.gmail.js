8228baa4d60e4807426855243e4be03481fb2877
HEMANT GUPTA <hemantgupta8800@gmail.com>
2026-01-29 19:49:23 +0530

    Changes in env files

--- taskora/backend/src/modules/auth/auth.gmail.js
+++ taskora/backend/src/modules/auth/auth.gmail.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  service: "hotmail",
+  service: "gmail",
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
