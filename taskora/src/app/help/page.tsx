aef15ac404e7a5bb730f349853550cff72047307
HEMANT GUPTA <hemantgupta8800@gmail.com>
2026-01-29 21:47:06 +0530

    Add pictures and major update in Help page

--- taskora/src/app/help/page.tsx
+++ taskora/src/app/help/page.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import Image from "next/image";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <>
+      <main className="min-h-screen bg-blue-100 ">
+        <div className="p-15 w-full  ">
+          <div className=" gird md:grid-cols-1 p-10 h-157 object-contain bg-white rounded-2xl shadow-2xl   ">
+            <Image
+              src="/help-center.avif"
+              alt="help center pic"
+              fill
+              className=" "
+            />
+          </div>
+        </div>
+      </main>
+    </>
+  );
 };
 
 export default page;
