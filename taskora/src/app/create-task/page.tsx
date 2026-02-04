3d4a000bb7df4f163daa69e120c82ffd08f9b0a8
HEMANT GUPTA <hemantgupta8800@gmail.com>
2026-02-04 15:21:47 +0530

    Add all components in create task page

--- taskora/src/app/create-task/page.tsx
+++ taskora/src/app/create-task/page.tsx
@@ -3,6 +3,12 @@
 import { useState } from "react";
 import Input from "../../components/ui/input";
 import { Textarea } from "../../components/ui/textarea";
+import { Calendar } from "../../components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "../../components/ui/popover";
 import {
   Select,
   SelectContent,
@@ -12,6 +18,8 @@ import {
 } from "../../components/ui/select";
 import { Button } from "../../components/ui/button";
 import { Checkbox } from "../../components/ui/checkbox";
+import { ChevronDownIcon, Upload } from "lucide-react";
+import { format } from "date-fns";
 
 export default function CreateTaskPage() {
   const [subtasks, setSubtasks] = useState([
@@ -19,6 +27,7 @@ export default function CreateTaskPage() {
     "Design UI mockup",
     "Get feedback & revisions",
   ]);
+  const [date, setDate] = useState<Date>();
 
   return (
     <>
@@ -32,7 +41,7 @@ export default function CreateTaskPage() {
           </p>
 
           <div className="mt-8 space-y-6">
-            <Input label="Task Title" defaultValue="Design Landing Page" />
+            <Input label="Task Title" placeholder="Design Landing Page" />
 
             <div className="space-y-1">
               <label
@@ -43,17 +52,17 @@ export default function CreateTaskPage() {
               </label>
               <Textarea
                 id="description"
-                defaultValue="Create a modern landing page design with responsive layout."
+                placeholder="Create a modern landing page design with responsive layout."
               />
             </div>
 
-            <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
+            <div className="grid grid-cols-1 gap-6 md:grid-cols-2 ">
               <div className="space-y-1">
-                <label className="text-sm font-medium text-gray-700">
+                <label className="text-md font-medium text-gray-700">
                   Priority
                 </label>
                 <Select defaultValue="high">
-                  <SelectTrigger>
+                  <SelectTrigger className="w-full md:w-[320px]">
                     <SelectValue placeholder="Select priority" />
                   </SelectTrigger>
                   <SelectContent>
@@ -65,13 +74,13 @@ export default function CreateTaskPage() {
               </div>
             </div>
 
-            <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
+            <div className="grid grid-cols-1 gap-6 ">
               <div className="space-y-1">
-                <label className="text-sm font-medium text-gray-700">
+                <label className="text-md font-medium text-gray-700">
                   Assign To
                 </label>
                 <Select defaultValue="michael">
-                  <SelectTrigger>
+                  <SelectTrigger className="w-full md:w-[320px]">
                     <SelectValue placeholder="Select assignee" />
                   </SelectTrigger>
                   <SelectContent>
@@ -82,11 +91,11 @@ export default function CreateTaskPage() {
                 </Select>
               </div>
               <div className="space-y-1">
-                <label className="text-sm font-medium text-gray-700">
+                <label className="text-md font-medium text-gray-700">
                   Category
                 </label>
                 <Select defaultValue="design">
-                  <SelectTrigger>
+                  <SelectTrigger className="w-full md:w-[320px]">
                     <SelectValue placeholder="Select category" />
                   </SelectTrigger>
                   <SelectContent>
@@ -96,6 +105,31 @@ export default function CreateTaskPage() {
                   </SelectContent>
                 </Select>
               </div>
+            </div>
+            <div className=" grid space-y-1">
+              <label className="text-md font-medium text-gray-700">
+                Due date
+              </label>
+              <Popover>
+                <PopoverTrigger asChild className="w-full md:w-[320px]">
+                  <Button
+                    variant="outline"
+                    data-empty={!date}
+                    className="data-[empty=true]:text-muted-foreground w-[212px] justify-between text-left font-normal"
+                  >
+                    {date ? format(date, "PPP") : <span>Pick a date</span>}
+                    <ChevronDownIcon />
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0" align="start">
+                  <Calendar
+                    mode="single"
+                    selected={date}
+                    onSelect={setDate}
+                    defaultMonth={date}
+                  />
+                </PopoverContent>
+              </Popover>
             </div>
 
             {/* Subtasks */}
@@ -114,24 +148,32 @@ export default function CreateTaskPage() {
             </div>
 
             {/* Attachments & Tags */}
-            <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
-              <div>
-                <h3 className="mb-2 text-sm font-medium text-gray-700">
-                  Attachments
-                </h3>
-                <Button variant="outline">Upload Files</Button>
-              </div>
 
-              <div>
-                <h3 className="mb-2 text-sm font-medium text-gray-700">Tags</h3>
-                <div className="flex flex-wrap gap-2">
-                  <Tag color="blue">UI/UX</Tag>
-                  <Tag color="pink">Landing Page</Tag>
-                  <Tag color="yellow">Urgent</Tag>
-                  <button className="text-sm font-medium text-blue-600 hover:underline">
-                    + Add Tag
-                  </button>
-                </div>
+            <div>
+              <h3 className="mb-2 text-sm font-medium text-gray-700">
+                Attachments
+              </h3>
+              <label className="h-50 w-50 border-2 border-dashed border-blue-400 rounded-md flex flex-col items-center justify-between cursor-pointer p-1 hover:bg-blue-50">
+                <Upload size={18} className="text-blue-600 mb-1 mt-18" />
+
+                <Input
+                  type="file"
+                  accept="image/*"
+                  className="hidden"
+                  label=""
+                />
+              </label>
+            </div>
+
+            <div className=" ">
+              <h3 className="mb-2 text-sm font-medium text-gray-700">Tags</h3>
+              <div className="flex flex-wrap gap-2">
+                <Tag color="blue">UI/UX</Tag>
+                <Tag color="pink">Landing Page</Tag>
+                <Tag color="yellow">Urgent</Tag>
+                <button className="text-sm font-medium text-blue-600 hover:underline">
+                  + Add Tag
+                </button>
               </div>
             </div>
 
