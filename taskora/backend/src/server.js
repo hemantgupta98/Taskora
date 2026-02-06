96cfc71235d3fa494eb51b0821814d66ca187e1f
HEMANT GUPTA <hemantgupta8800@gmail.com>
2026-02-06 22:11:13 +0530

    Work on scoket.io connection

--- taskora/backend/src/server.js
+++ taskora/backend/src/server.js
@@ -1,12 +1,34 @@
 import app from "./app.js";
 import connectDB from "./config/db.js";
 import dotenv from "dotenv";
+import http from "http";
+import { Server } from "socket.io";
+
 dotenv.config();
 
 const port = process.env.PORT || 5000;
 
 connectDB();
 
-app.listen(port, () => {
-  console.log(`server running on ${port}`);
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("🟢 Socket connected:", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("🔴 Socket disconnected:", socket.id);
+  });
+});
+
+app.set("io", io);
+
+server.listen(port, () => {
+  console.log(`🚀 Server running on port ${port}`);
 });
