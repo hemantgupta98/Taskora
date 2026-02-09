17155882dd2f620d4635f2ac35a1fb8d58767361
HEMANT GUPTA <hemantgupta8800@gmail.com>
2026-02-09 10:58:40 +0530

    Create a plans schema

---
+++ taskora/backend/src/modules/plans/plans.model.js
@@ -0,0 +1,17 @@
+import mongoose, { model } from "mongoose";
+
+const planSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    board: { type: String, required: true },
+    work: { type: String, required: true },
+    startDate: { type: Number, required: true },
+    access: { type: String, required: true },
+    dueDate: { type: Number, required: true },
+  },
+  { timestamps: true },
+);
+
+const planModel = mongoose.model("createPlans", planSchema);
+
+export default planModel;
