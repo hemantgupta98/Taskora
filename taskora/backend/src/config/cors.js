cb731ad95218550561ad1dc8368caca47a0163e1
HEMANT GUPTA <hemantgupta8800@gmail.com>
2026-02-21 20:08:48 +0530

    add frontend url

--- taskora/backend/src/config/cors.js
+++ taskora/backend/src/config/cors.js
@@ -1,5 +1,5 @@
 const corsOption = {
-  origin: "http://localhost:3000",
+  origin: ["http://localhost:3000", "https://taskora-peach.vercel.app"],
   optionsSuccessStatus: 200,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD"],
   credentials: true,
