c6f1af2c3d1e42651095d31e9905edd721db491b
HEMANT GUPTA <hemantgupta8800@gmail.com>
2026-02-21 17:06:14 +0530

    Multer setup complete on backend

--- taskora/backend/src/app.js
+++ taskora/backend/src/app.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import cors from "cors";
-
+import path from "path";
 import passport from "passport";
 import corsOption from "./config/cors.js";
 import allRoutes from "./routes/index.js";
@@ -18,6 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors(corsOption));
 app.use(passport.initialize());
 
+app.use("/uploads", express.static(path.join("uploads")));
 app.use("/api", allRoutes);
 
 export default app;
--- taskora/backend/src/modules/acceptInvite/accept.routes.js
+++ taskora/backend/src/modules/acceptInvite/accept.routes.js
@@ -5,6 +5,7 @@ import {
   deleteTeamMemeber,
 } from "./accept.contoller.js";
 import express from "express";
+import upload from "./accept.upload.js";
 
 const router = express.Router();
 
@@ -12,5 +13,11 @@ router.post("/acceptinvite", accept);
 router.get("/", getAcceptUsers);
 router.get("/team-member", getInvitedTeamMember);
 router.delete("/deleteplans/:id", deleteTeamMemeber);
+router.post("/upload", upload.single("image"), (req, res) => {
+  res.json({
+    message: "Image uploaded successfully",
+    file: req.file,
+  });
+});
 
 export default router;
