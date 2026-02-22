a87aee664f258438f24c92f82ce0afd1691f2330
HEMANT GUPTA <hemantgupta8800@gmail.com>
2026-02-22 07:41:03 +0530

    add api connection

--- taskora/src/lib/api.ts
+++ taskora/src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "https://taskora-88w5.onrender.com/api",
   withCredentials: true,
 });
